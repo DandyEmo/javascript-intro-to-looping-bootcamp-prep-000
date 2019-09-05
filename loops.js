5dbb7b0dfdfc6b8b0cf89779fd967f43063ad12d
kevin Abreu <kabreu11385@gmail.com>
2019-09-05 22:39:40 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,38 @@
+function forLoop(arr) {
+  
+for (let i = 0; i < 25; i++) {
+  if (i == 1) {
+  arr.push("I am 1 strange loop.")
+  } else {
+    arr.push(`I am ${i} strange loops.`)
+    }
+ }
+ 
+ return arr;
+}
+
+function whileLoop(n) {
+     while(n > 0) {
+        console.log(n)
+        n -= 1
+     }
+     return 'done'
+     
+}
+
+
+function incrementVariable(val) {
+  val++;
+  return val;
+}
+
+function doWhileLoop(num) {
+  var i = 0;
+  
+  do {
+      console.log("I run once regardless.");
+    } while (incrementVariable(i)< num); i++;
+    
+}
+
+
